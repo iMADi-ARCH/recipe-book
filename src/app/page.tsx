e900e5b48e6d8e6d7dee89d73d3332381ac1f406
iMADi-ARCH <nandanaditya985@gmail.com>
2023-05-09 14:57:21 +0530

    hotfix: Kv

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-import kv from "@vercel/kv";
-
 export default async function Home() {
-    await kv.set("user_1_session", "session_token_value");
-    const session = await kv.get("user_1_session");
     return <main></main>;
 }
